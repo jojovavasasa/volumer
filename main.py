9a1d1d00263e387708f211c10b2ac78ed8e17802
jojovavasasa <106984298+jojovavasasa@users.noreply.github.com>
2026-04-18 16:51:51 +0200

    added cone

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import time
 
 unit = input("Welke eenheid ga je gebruiken? ")
-shape = input("Welke vorm wil je berekenen?\n(A): Piramide\n(B): Balk\n(C): Kubus\n(D): Bol\n")
+shape = input("Welke vorm wil je berekenen?\n(A): Piramide\n(B): Balk\n(C): Kubus\n(D): Bol\n(E): Kegel\n")
 shape = shape.lower()
 if shape == "a":
     length = float(input(f"Wat is de lengte in {unit}? "))
@@ -18,8 +18,13 @@ elif shape == "c":
     length = float(input(f"Wat is de lengte in {unit}? "))
     volume = length * length * length
 elif shape == "d":
-    radius = float(input(f"Wat is de radius in {unit}? "))
+    radius = float(input(f"Wat is de straal in {unit}? "))
     volume = (4/3) * math.pi * (radius * radius * radius)
+elif shape == "d":
+    radius = float(input(f"Wat is de straal in {unit}? "))
+    height = float(input(f"Wat is de hoogte in {unit}? "))
+    volume = (1/3) * math.pi * (radius * radius) * height
+
 print(f"{volume} {unit}³")
 print(f"{round(volume)} {unit}³ afgerond")
 time.sleep(5)
